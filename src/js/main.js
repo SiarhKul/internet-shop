e14998b91a899ba1b7dd0705e6d302b6494f78ff
SiarhKul <SiarhKul@gmail.com>
2021-02-16 21:40:55 +0300

    init:create tamplate

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+import $ from '../local_modules/jquery/dist/jquery.min';
+
+$(document).ready(() => {
+    // eslint-disable-next-line no-console
+    console.log(`document ready`);
+})
+
+[1, 2, 3].map(num => num * 2);
+
+console.log("sdf");
