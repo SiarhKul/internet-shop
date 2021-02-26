7e57852d8544b0c3d965a0dac0e7323a21ae3e96
SiarhKul <SiarhKul@gmail.com>
2021-02-26 16:16:14 +0300

    feat: create invisible footer card

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,3 @@
-// import $ from '../local_modules/jquery/dist/jquery.min';
-// import '../local_modules/slick-carousel/slick/slick.min';
-
 import $ from 'jquery';
 import 'slick-carousel';
 
@@ -12,9 +9,19 @@ jQuery(() => {
     dots: true,
     infinite: true,
     centerPadding: '50px',
-    // speed: 500,
-    // fade: true,
-    // cssEase: 'linear',
     arrows: false,
   });
 });
+
+const cardMobilephone = document.querySelector('.card-mobilephone ');
+
+cardMobilephone.addEventListener('click', (e) => {
+  e.stopPropagation();
+  console.log(e.target.children[4]);
+});
+// cardMobilephone.addEventListener('click', (e) => {
+//   const footer = document.querySelector('.footer');
+//   footer.classList.toggle('footer-show');
+//   console.log(e.target);
+//   console.log(footer);
+// });
